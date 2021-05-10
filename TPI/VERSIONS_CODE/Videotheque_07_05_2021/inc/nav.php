028b3b699968d62fedc48dd235f619a04d3ad49a
twinh0 <71710478+twinh0@users.noreply.github.com>
2021-05-10 10:48:06 +0200

    Add files via upload

---
+++ TPI/VERSIONS_CODE/Videotheque_07_05_2021/inc/nav.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../views/accueil.php">
+                <img src="https://raw.githubusercontent.com/twinh0/TPI_2021/main/TPI/RESSOURCES/LOGO_V2.png" alt="" width="30" height="30" class="d-inline-block align-text-bottom">
+                Vidéothèque
+            </a>
+            <!-- ONGLETS CLICKABLES -->
+            <div class="d-flex navbar-nav" id="navbarNavDarkDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" aria-current="page" href="../views/accueil.php">Accueil</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Forum
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                            <li><a class="dropdown-item" href="../views/forum.php">Voir post</a></li>
+                            <li><a class="dropdown-item" href="../views/create_post.php">Créer post</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Compte
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                            <li><a class="dropdown-item" href="../views/register.php">Login/Register</a></li>
+                            <li><a class="dropdown-item" href="../views/profil.php">Profil</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" aria-current="page" href="../views/deconnexion.php">Déconnexion</a>
+                    </li>
+            </div>
+    </nav>
---
+++ TPI/VERSIONS_CODE/Videotheque_07_05_2021/views/forum.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+require("../inc/db_requetes.php");
+//EN-TÊTE//NAVBAR
+include_once("../inc/nav.php");
+var_dump($_SESSION);
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="../css/style.css">
+    <title>Forum</title>
+</head>
+
+<body>
+    <!-- SOUS-TITRE -->
+    <h1 class="text-center" style="padding-top: 50px;">Forum</h1>
+
+
+
+    <!-- JAVASCRIPT ADDITIONNEL -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
